6d2bdbecb4b72fa54c0ccd26b13ee536af4a5760
Michael Edvalson <michael.edvalson@gmail.com>
2021-09-05 21:41:12 -0600

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Do Not Edit.  Ever.
         </p>
         <a
           className="App-link"
