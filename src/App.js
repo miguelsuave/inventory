206a7391c5ce6f693497e5d104ad5f714a2ba44d
Michael Edvalson <michael.edvalson@gmail.com>
2021-09-05 22:06:54 -0600

    Added initial scanner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import BarcodeScannerComponent from "react-webcam-barcode-scanner";
 
 function App() {
+  const [ data, setData ] = React.useState('Not Found');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Do Not Edit.  Ever.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text)
+          else setData('Not Found')
+        }}
+      />
+      <p>{data}</p>
+    </>
+  )
 }
 
 export default App;
